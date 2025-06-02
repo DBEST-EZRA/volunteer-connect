9eba9ad91332ad6a72c196dd66c3b1e3dc6252d2
DBEST-EZRA <ezradbest101@gmail.com>
2025-06-02 12:01:48 +0300

    fixed horizontal scrolling

--- src/Pages/HeroSection.jsx
+++ src/Pages/HeroSection.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import heroImage from "../assets/why us.jpg";
 
@@ -7,8 +6,9 @@ const HeroSection = () => {
     <section
       className="d-flex align-items-center justify-content-center text-white position-relative"
       style={{
-        width: "100vw",
+        width: "100%",
         height: "75vh",
+        overflowX: "hidden",
         backgroundImage: `url(${heroImage})`,
         backgroundSize: "cover",
         backgroundPosition: "center",
@@ -22,12 +22,23 @@ const HeroSection = () => {
       ></div>
 
       {/* Text Content */}
-      <div className="position-relative text-center z-1">
+      <div className="position-relative text-center z-1 px-3">
         <small className="text-uppercase fw-semibold">
           GET ON THE RIGHT WAY
         </small>
         <h1 className="display-4 fw-bold">Transforming Workplaces</h1>
       </div>
+
+      {/* Responsive height override */}
+      <style>
+        {`
+          @media (max-width: 768px) {
+            section {
+              height: 50vh !important;
+            }
+          }
+        `}
+      </style>
     </section>
   );
 };
